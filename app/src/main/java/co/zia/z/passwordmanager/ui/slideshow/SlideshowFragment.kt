6f44380ebc4493817ea2dc579c6e6d2becc73886
Zia Khalid <ziakhalid@Zias-MacBook-Pro.local>
2019-10-24 11:50:25 +0800

    Initial Project

---
+++ app/src/main/java/co/zia/z/passwordmanager/ui/slideshow/SlideshowFragment.kt
@@ -0,0 +1,31 @@
+package co.zia.z.passwordmanager.ui.slideshow
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import co.zia.z.passwordmanager.R
+
+class SlideshowFragment : Fragment() {
+
+    private lateinit var slideshowViewModel: SlideshowViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        slideshowViewModel =
+            ViewModelProviders.of(this).get(SlideshowViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val textView: TextView = root.findViewById(R.id.text_slideshow)
+        slideshowViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
